314c26ac95f96dd864510fb1f3d3b00a01465cc5
luctst <lucas.tostee@gmail.com>
2020-10-05 22:14:13 +0200

    Fix prettier errors

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,13 +1,13 @@
 import React from "react";
-import {Container} from "../styles/Container";
+import { Container } from "../styles/Container";
 import styled from "styled-components";
 
 const SCHeader = styled(Container)``;
 
 export default function () {
-    return (
-        <SCHeader as="header">
-            <h1>Hello</h1>
-        </SCHeader>
-    );
+  return (
+    <SCHeader as="header">
+      <h1>Hello</h1>
+    </SCHeader>
+  );
 }
