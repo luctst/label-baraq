f67d5c66625ddbe82b9dab2cdceb36c429575598
luctst <lucas.tostee@gmail.com>
2020-10-06 14:18:31 +0200

    Add Header component in main component because it will be called everytime

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
+import Header from "./components/Header";
 import Home from "./views/Home";
 import * as serviceWorker from "./serviceWorker";
 
@@ -9,10 +10,13 @@ const themes = {
   mainColor: "#fff",
 };
 
+document.body.style = "margin: 0; padding: 0;";
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider theme={themes}>
+        <Header />
         <Switch>
           <Route exact path="/" component={Home} />
         </Switch>
