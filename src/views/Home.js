536f908bb56890283693efb40bb562416e890511
luctst <lucas.tostee@gmail.com>
2020-10-06 14:21:18 +0200

    Update

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,13 +1,7 @@
 import React from "react";
-import { Container } from "../styles/Container";
-import styled from "styled-components";
-
-const SCHeader = styled(Container)``;
 
 export default function () {
   return (
-    <SCHeader as="header">
-      <h1>Hello</h1>
-    </SCHeader>
+    <main></main>
   );
 }
