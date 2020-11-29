3d3df22c455f7eca91f97b9f81aaea9005b388ce
Antoine Chassagne <antoine.chassagne@me.com>
2020-11-29 21:38:35 +0100

    Fix cgu layout

--- src/views/Cgu.jsx
+++ src/views/Cgu.jsx
@@ -9,16 +9,17 @@ const Cgu = () => {
     <>
       <BaseHeader />
       <main className="cgu container">
-        <p className="text-center">Informations générales</p>
-        <p className="paragraph text-center mb--10">Rapin Clémentine</p>
-        <p className="text-center">2 passage du chantier 75012 Paris</p>
-        <p className="paragraph text-center mb--10">0660543051</p>
-        <p className="text-center">Email labelbaraque@gmail.com</p>
-        <p className="paragraph text-center">N° SIRET : 849 745 443 00010</p>
-        <p className="text-center">Hébergeur du site : Vercel</p>
-        <p className="paragraph text-center mb--100">
+        <p className="text-bold mb--10">Informations générales</p>
+        <p className="paragraph  mb--30">Rapin Clémentine</p>
+        <p>2 passage du chantier 75012 Paris</p>
+        <p className="paragraph  mb--30">0660543051</p>
+        <p>Email labelbaraque@gmail.com</p>
+        <p className="paragraph ">N° SIRET : 849 745 443 00010</p>
+        <p>Hébergeur du site : Vercel</p>
+        <p className="paragraph  mb--100">
           Caractères des informations personnelles collectées
         </p>
+        <p className="text-bold mt--50">Mentions légales</p>
         <p className="paragraph">
           La loi 78-87 du 6 janvier 1978, la loi 2004-801 du 6 août 2004 ainsi que
           l'article L. 226-13 du Code Pénal et la directive Européenne du 24 Octobre 1995
@@ -28,51 +29,50 @@ const Cgu = () => {
           Au cours de la navigation sur le Site, et dans le cadre d'inscription à des
           accès spécifiques du Site, les données personnelles suivantes sont enregistrées
           :
-          <ul>
-            <li>- Nom et Prénom</li>
-            <li>- Adresse électronique</li>
-          </ul>
-          L'utilisateur est informé de la possibilité de demander au webmaster du site à
-          l'adresse suivante : labelbaraque@gmail.com :
-          <ul>
-            <li>
-              - la vérification des données personnelles ayant fait l'objet d'une saisie
-              pour son compte
-            </li>
-            <li>
-              - les informations ayant trait aux finalités de traitement des données
-            </li>
-            <li>
-              - Les informations ayant trait à l'identité et au secteur géographique des
-              possesseurs de ces données
-            </li>
-            <li>
-              - la communication d'une copie de ces données à ce titre gratuit dans la
-              mesure ou celle-ci n'est pas abusive dans son caractère répétitif ou
-              disproportionné
-            </li>
-          </ul>
-          Aucune information personnelle n'est collectée ni publiée ou cédée à titre
-          gratuit ou onéreux à l'insu de l'utilisateur. Clementine Rapin est autorisé à
-          effectuer des analyses statistiques sur l'utilisation et la typologie des
-          utilisateurs sous réserve de préserver l'anonymat de ces derniers. Droit de
-          modification des informations collectées Selon les dispositions de l'article 34
-          de la loi 44-87 du 6 janvier 1978, l'utilisateur dispose d'un droit de
-          modification des données collectées le concernant. Dans ce but, il devra envoyer
-          un courrier postale à l’adresse de ME. En précisant ses coordonnées personnelles
-          et les références de son compte sous lequel sont enregistrées ses données à
-          caractère personnelles. Les modifications demandées interviendront dans un délai
-          de quatre à six semaines.
         </p>
+        <ul className="mb--16">
+          <li>- Nom et Prénom</li>
+          <li>- Adresse électronique</li>
+        </ul>
+        <p>
+          L'utilisateur est informé de la possibilité de demander au webmaster du site à
+          l'adresse suivante :
+        </p>
+        <p>labelbaraque@gmail.com</p>
+        <ul>
+          <li>
+            - la vérification des données personnelles ayant fait l'objet d'une saisie
+            pour son compte
+          </li>
+          <li>- les informations ayant trait aux finalités de traitement des données</li>
+          <li>
+            - Les informations ayant trait à l'identité et au secteur géographique des
+            possesseurs de ces données
+          </li>
+          <li>
+            - la communication d'une copie de ces données à ce titre gratuit dans la
+            mesure ou celle-ci n'est pas abusive dans son caractère répétitif ou
+            disproportionné
+          </li>
+        </ul>
+        <p className="mb--30">
+          Aucune information personnelle n'est collectée ni publiée ou cédée à titre
+          gratuit ou onéreux à l'insu de l'utilisateur.
+        </p>
+        <p className="mb--30">
+          Clementine Rapin est autorisé à effectuer des analyses statistiques sur
+          l'utilisation et la typologie des utilisateurs sous réserve de préserver
+          l'anonymat de ces derniers.
+        </p>
+        <p className="text-bold">Droit de modification des informations collectées</p>
         <p className="paragraph">
-          Un "cookie" permet l'identification de l'utilisateur, la personnalisation de sa
-          consultation du site et le chargement plus rapide des pages du site à l'aide
-          d'un chargement d'un fichier de données sur son ordinateur. L'utilisateur
-          reconnaît être informé de cette pratique et autorise Clémentine Rapin à procéder
-          ainsi. Sauf cas spécial de réquisition légale, Clémentine Rapin s'engage à ne
-          jamais communiquer le contenu de ces cookies à des tiers ou sociétés partenaires
-          ou clientes. Tout utilisateur peut refuser l'enregistrement de ces cookies ou
-          configurer son navigateur au préalable qui l'avertira.
+          Selon les dispositions de l'article 34 de la loi 44-87 du 6 janvier 1978,
+          l'utilisateur dispose d'un droit de modification des données collectées le
+          concernant. Dans ce but, il devra envoyer un courrier postale à l’adresse de ME.{" "}
+          <br />
+          En précisant ses coordonnées personnelles et les références de son compte sous
+          lequel sont enregistrées ses données à caractère personnelles. Les modifications
+          demandées interviendront dans un délai de quatre à six semaines.
         </p>
       </main>
     </>
